30b5f5a45d4df2a94d4d360a6f82ba5706967e59
Andrew Fore <andrew.fore@newfold.com>
2024-01-05 22:37:47 -0500

    More updates

--- nvim/lua/andyfore/core/init.lua
+++ nvim/lua/andyfore/core/init.lua
@@ -1,2 +1,4 @@
+vim.api.nvim_set_option("clipboard","unnamed")
+
 require("andyfore.core.keymaps")
 require("andyfore.core.options")
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require 'wezterm'
 -- This table will hold the configuration.
 local config = {}
 
+local act = wezterm.action
+
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
@@ -14,7 +16,8 @@ end
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'nord'
+-- config.color_scheme = 'nord'
+config.color_scheme = 'Catppuccin Mocha'
 
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -23,6 +26,42 @@ config.initial_rows = 45
 
 config.font =
   wezterm.font('JetBrainsMono Nerd Font')
+
+-- Window size position and decoration
+config.window_decorations = "RESIZE"
+config.keys = {
+  -- Switch to the default workspace
+  {
+    key = 'y',
+    mods = 'CTRL|SHIFT',
+    action = act.SwitchToWorkspace {
+      name = 'default',
+    },
+  },
+  -- Switch to a monitoring workspace, which will have `top` launched into it
+  {
+    key = 'u',
+    mods = 'CTRL|SHIFT',
+    action = act.SwitchToWorkspace {
+      name = 'monitoring',
+      spawn = {
+        args = { 'top' },
+      },
+    },
+  },
+  -- Create a new workspace with a random name and switch to it
+  { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
+  -- Show the launcher in fuzzy selection mode and have it list all workspaces
+  -- and allow activating one.
+  {
+    key = '9',
+    mods = 'ALT',
+    action = act.ShowLauncherArgs {
+      flags = 'FUZZY|WORKSPACES',
+    },
+  },
+  { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+}
 
 -- and finally, return the configuration to wezterm
 return config
